bfe0a2fde34fcf873ba79d0c190eec05be247b32
root <root@linux-test.mytwoquarters.com>
2025-09-03 11:03:36 +0200

    added get tld script

---
+++ scripts/get-tld.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# variables
+work_dir="$(pwd)"
+sources_path="$work_dir/../sources/tld-sources.txt"
+temp_dir="$work_dir/../temp/"
+tld_source_file="tld_source.txt"
+tld_file="tld.txt"
+tld_source_path="$temp_dir$tld_source_file"
+tld_path="$work_dir/../tld/$tld_file"
+tld_whitelist="^com$|^de$|^net$|^org$"
+
+# creating and cleanup temp dir
+mkdir -p "$temp_dir"
+rm -f "$temp_dir"*
+
+# download the tld source list
+wget -i "$sources_path" -O "$tld_source_path"
+
+# create the header for the adblock plus list
+cat > "$tld_path" << EOL
+[Adblock Plus]
+! Title: TLD list
+! Description: Blocks TLDs.
+! Syntax: AdBlock
+!
+!
+EOL
+
+# process the downloaded tld source list and append to the tld list
+
+cat "$tld_source_path" | grep -v '^#' | tr '[:upper:]' '[:lower:]' | grep -vE "$tld_whitelist" | sed 's/^/||/' | sed 's/$/^/' | tee -a "$tld_path"
